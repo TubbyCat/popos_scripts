fe5fcd57d3665b5f27253b3cbf4738d3cb67edef
TubbyCat <100334720+TubbyCat@users.noreply.github.com>
2022-07-25 02:38:32 -0400

    Update systemd_declutter.sh

--- systemd_declutter.sh
+++ systemd_declutter.sh
@@ -1,17 +1,19 @@
 #!/bin/env bash 
 # stops & masks unused systemd services.
 # may need to modify this to your needs! this will drop services such as cups (printing). 
+##  WIP  ##
 
 STOP="systemctl stop --now"
 MASK="systemctl mask --now"
-
+#LIST-OPTIONAL="
+#LIST-ACTUAL="avahi-daemon.socket avahi-daemon.service dmesg.service dmesg.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service "
 if [[ $UID -ne 0 ]]; then echo 'ROOT! otherwise rm ./systemd_declutter.sh' && exit 1
 fi 
 
 
-$STOP  avahi-daemon.socket avahi-daemon.service  dmesg cups.path cups-browsed.service cups.service cups.socket ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service 
+$STOP  avahi-daemon.socket avahi-daemon.service colord.service dmesg cups.path cups-browsed.service cups.service cups.socket ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service 
 
-$MASK  avahi-daemon.service avahi-daemon.socket dmesg.service cups.path cups.socket cups.service cups-browsed.service avahi-daemon.socket avahi-daemon.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service
+$MASK  avahi-daemon.service avahi-daemon.socket colord.service dmesg.service cups.path cups.socket cups.service cups-browsed.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service
 
 # if not using wifi uncomment
 $STOP wpa_supplicant.service
