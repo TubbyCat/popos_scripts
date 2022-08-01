4b9b22cffc0dfcf4a3b643a1487f2886acf5b6d9
TubbyCat <100334720+TubbyCat@users.noreply.github.com>
2022-08-01 15:45:07 -0400

    Update systemd_declutter.sh

--- systemd_declutter.sh
+++ systemd_declutter.sh
@@ -1,23 +1,20 @@
 #!/bin/env bash 
 # stops & masks unused systemd services.
 # may need to modify this to your needs! this will drop services such as cups (printing). 
-##  WIP  ##
 
 STOP="systemctl stop --now"
 MASK="systemctl mask --now"
-#LIST-OPTIONAL="
+#LIST-OPTIONAL="colord.service cups.path cups-browsed.service cups.service cups.socket wpa_supplicant.service "
 #LIST-ACTUAL="avahi-daemon.socket avahi-daemon.service dmesg.service dmesg.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service "
 if [[ $UID -ne 0 ]]; then echo 'ROOT! otherwise rm ./systemd_declutter.sh' && exit 1
 fi 
 
-
 $STOP  avahi-daemon.socket avahi-daemon.service colord.service dmesg cups.path cups-browsed.service cups.service cups.socket ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service 
-
 $MASK  avahi-daemon.service avahi-daemon.socket colord.service dmesg.service cups.path cups.socket cups.service cups-browsed.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service
 
 # if not using wifi uncomment
-$STOP wpa_supplicant.service
-$MASK wpa_supplicant.service
+#$STOP wpa_supplicant.service
+#$MASK wpa_supplicant.service
 
 #If not using touchpad e.g. if not laptop or keyboard + touchpad user uncomment 
 # $STOP touchegg.service && $MASK touchegg.service
