ee2dca7025c1bf93c46e7216f979a26d5840dce6
TubbyCat <100334720+TubbyCat@users.noreply.github.com>
2022-08-03 07:08:28 -0400

    Update systemd_declutter.sh

--- systemd_declutter.sh
+++ systemd_declutter.sh
@@ -2,19 +2,26 @@
 # stops & masks unused systemd services.
 # may need to modify this to your needs! this will drop services such as cups (printing). 
 
-STOP="systemctl stop --now"
-MASK="systemctl mask --now"
-#LIST-OPTIONAL="colord.service cups.path cups-browsed.service cups.service cups.socket wpa_supplicant.service "
-#LIST-ACTUAL="avahi-daemon.socket avahi-daemon.service dmesg.service dmesg.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service "
-if [[ $UID -ne 0 ]]; then echo 'ROOT! otherwise rm ./systemd_declutter.sh' && exit 1
+if [[ $UID -ne 0 ]]; then 
+echo 'ROOT! otherwise rm ./systemd_declutter.sh' && exit 1
 fi 
 
-$STOP  avahi-daemon.socket avahi-daemon.service colord.service dmesg cups.path cups-browsed.service cups.service cups.socket ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service 
-$MASK  avahi-daemon.service avahi-daemon.socket colord.service dmesg.service cups.path cups.socket cups.service cups-browsed.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service
+STOP="systemctl stop --now"
+MASK="systemctl mask --now"
+LIST-ACTUAL="avahi-daemon.socket avahi-daemon.service dmesg.service ubuntu-advantage-cloud-id-shim.service ubuntu-advantage.service"
+LIST-OPT="colord.service cups.path cups-browsed.service cups.service cups.socket"
 
-# if not using wifi uncomment
-#$STOP wpa_supplicant.service
-#$MASK wpa_supplicant.service
+$STOP  $LIST-ACTUAL && $MASK $LIST-ACTUAL
+$STOP $LIST-OPTIONAL && $MASK $LIST-OPTIONAL 
+
+# if NOT using wifi uncomment
+# $STOP wpa_supplicant.service && $MASK wpa_supplicant.service
 
 #If not using touchpad e.g. if not laptop or keyboard + touchpad user uncomment 
 # $STOP touchegg.service && $MASK touchegg.service
+
+systemctl --now daemon-reload && systemctl --now daemon reexec
+
+echo "All done. As a parting gift, a small pictoral horror story..."
+systemd-analyze security
+exit 0
